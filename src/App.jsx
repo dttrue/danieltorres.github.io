62ed78e9006748fbdc663ad744c678601882f5ab
dttrue <danieltorres@pursuit.org>
2023-11-19 01:58:43 -0500

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+
+
+const App = () => {
+  return <>
+    <Navbar />
+    <Hero />
+    <Skills />
+    <About />
+  </>
+};
+
+export default App;
