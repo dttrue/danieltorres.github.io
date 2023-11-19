437272ee37dcd255a0c8017bb91efc7cef7e162d
dttrue <danieltorres@pursuit.org>
2023-11-19 02:23:45 -0500

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
     <Hero />
     <Skills />
     <About />
+    <Projects />
   </>
 };
 
